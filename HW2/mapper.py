5d0dbe5d18ebf9f412bb5b1735871cafd0225396
Henry <henryliang818@gmail.com>
2023-05-04 14:11:35 -0500

    Complete implementation of map reduce pipeline

---
+++ HW2/mapper.py
@@ -0,0 +1,37 @@
+import sys
+
+# Mapper
+substrings = ['nu', 'chi', 'haw']
+for line in sys.stdin:
+    # Remove leading and trailing whitespace and split the words
+    fields = line.strip().split()
+    
+    if len(fields) < 4:
+        continue
+    elif len(fields) == 4: # 1-gram
+        word = fields[0]
+        try:
+            year = int(fields[1])
+        except:
+            continue
+        occurence = int(fields[2])
+        volumes = int(fields[3])
+    elif len(fields) == 5: # 2-grams
+        word = fields[0]
+        second_word = fields[1]
+        try:
+            year = int(fields[2])
+        except:
+            continue
+        occurence = int(fields[3])
+        volumes = int(fields[4])
+    # print(fields)
+    for substring in substrings:
+        if len(fields) == 4:
+            count = int(substring in word)
+            if count > 0:
+                print(f'{year},{substring},{count * volumes}')
+        elif len(fields) == 5:
+            count = int(substring in word) + int(substring in second_word)
+            if count > 0:
+                print(f'{year},{substring},{count * volumes}')
---
+++ HW2/reducer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import sys
+
+# Read the entire line from STDIN
+current_year = None
+current_substring = None
+current_volumes = 0
+current_count = 0
+for line in sys.stdin:
+    # Remove leading and trailing whitespace
+    line = line.strip()
+
+    # Split the line into fields
+    year, substring, volumes = line.split(',')
+
+    # Convert volumes to int
+    try:
+        volumes = int(volumes)
+    except ValueError:
+        continue
+
+    # If the current year and substring match, accumulate the volumes
+    if current_year == year and current_substring == substring:
+        current_volumes += volumes
+        current_count += 1
+    else:
+        if current_year and current_substring:
+            average_volumes = current_volumes / current_count
+            print(f"{current_year},{current_substring},{average_volumes}")
+        current_year = year
+        current_substring = substring
+        current_volumes = volumes
+        current_count = 1
+
+# Output the last year and substring if needed
+if current_year and current_substring:
+    average_volumes = current_volumes / current_count
+    print(f"{current_year},{current_substring},{average_volumes}")
